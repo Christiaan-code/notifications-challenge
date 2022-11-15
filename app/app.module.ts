fe27399deba6507d1e0c96d38acff058b5b37e28
Christiaan-code <72527153+Christiaan-code@users.noreply.github.com>
2022-11-15 08:55:57 +0200

    header template init

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PopupModalComponent } from './examples/popup-modal/popup-modal.component';
+import { NotificationListComponent } from './components/notification-list/notification-list.component';
+import { NotificationItemComponent } from './components/notification-list/notification-item/notification-item.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , NgbModule.forRoot()],
-  declarations: [ AppComponent, PopupModalComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    NotificationListComponent,
+    NotificationItemComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
